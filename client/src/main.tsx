73b01199f4bf24dfa5f3df5f30d9bb4845560410
Kaveh <74491598+KavehHashemi@users.noreply.github.com>
2023-05-29 16:08:49 +0330

    started implementing Auth0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store/store.ts";
+import { Auth0ProviderWithNavigate } from "./config/Auth0Provider.tsx";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -12,9 +14,13 @@ const client = new ApolloClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <App></App>
-    </Provider>
-  </ApolloProvider>
+  <BrowserRouter>
+    <Auth0ProviderWithNavigate>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <App></App>
+        </Provider>
+      </ApolloProvider>
+    </Auth0ProviderWithNavigate>
+  </BrowserRouter>
 );
